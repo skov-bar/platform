a8723c3341382776a7c9d7a6f7d6e655a020f6d9
Nikita Barskov <nbarskov@gmail.com>
2024-02-18 22:54:07 +0100

    Add branch protection and default branch

--- infrastructure/opentofu/repositories.tf
+++ infrastructure/opentofu/repositories.tf
@@ -4,6 +4,27 @@ import {
 }
 
 resource "github_repository" "platform" {
-  name = "platform"
+  name       = "platform"
   visibility = "public"
+}
+
+import {
+  to = github_branch_default.platform_main
+  id = "platform"
+}
+
+resource "github_branch_default" "platform_main" {
+  repository = github_repository.platform.name
+  branch     = "main"
+}
+
+resource "github_branch_protection" "platform" {
+  repository_id = github_repository.platform.node_id
+  pattern       = "main"
+  required_status_checks {
+    strict = true
+    contexts = [
+      "spacelift/platform",
+    ]
+  }
 }
